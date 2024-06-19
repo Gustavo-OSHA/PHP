54da27d9ede5c0b4381577d2b7b967ab39354063
Gustavo de Oliveira Silva Hilario Amorim <guhilario@gmail.com>
2024-06-19 14:22:47 -0300

    .

--- index.php
+++ index.php
@@ -16,13 +16,16 @@
         <div class="conteudo">
             <nav class="modulos">
                 <div class="modulo verde">
-                    <h3>Módulo 01 </h3>
+                    <h3>Básico</h3>
                     <ul>
-                        <li><a href="exercicio.php">Exercício A</a></li>                        
+                        <li>
+                        <a href="exercicio.php?dir=basico&file=ola">
+                                Olá PHP
+                            </a>
+                        </li>                        
                     </ul>
                 </div>
-                            </nav>
-
+            </nav>
         </div>
     </main>
     <footer class="rodape">
