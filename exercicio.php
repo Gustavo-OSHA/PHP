9beca8f3be8741ee738adb930301fd453caced61
Gustavo de Oliveira Silva Hilario Amorim <guhilario@gmail.com>
2024-06-19 11:49:09 -0300

    Navegacao

--- exercicio.php
+++ exercicio.php
@@ -4,15 +4,20 @@
     <meta charset="UTF-8">
     <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
     <link rel="stylesheet" href="assets\css\style.css">
+    <link rel="stylesheet" href="assets\css\exercicio.css">
     <title>Exercício</title>
     
 </head>
-<body>
+<body class="exercicio">
     <header class="cabecalho">
         <h1>Curso PHP</h1>
         <h2>Visualização Exercício</h2>
     </header>
-    <main class="principal">
+    <nav class="navegacao">
+        <a href="#" class="verde">Sem formatação</a>
+        <a href="index.php" class="vermelho">Voltar</a>
+    </nav>
+    <main class="principal">        
         <div class="conteudo">
             
 
