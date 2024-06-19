eb6292d294065c69fb4f20d87c489387b588b490
Gustavo de Oliveira Silva Hilario Amorim <guhilario@gmail.com>
2024-06-19 11:30:41 -0300

    Visualizaçao do Exercicio

---
+++ exercicio.php
@@ -0,0 +1,25 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <link href="https://fonts.googleapis.com/css2?family=Oswald:wght@200..700&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="assets\css\style.css">
+    <title>Exercício</title>
+    
+</head>
+<body>
+    <header class="cabecalho">
+        <h1>Curso PHP</h1>
+        <h2>Visualização Exercício</h2>
+    </header>
+    <main class="principal">
+        <div class="conteudo">
+            
+
+        </div>
+    </main>
+    <footer class="rodape">
+        CODE3R & ALUNOS <?= date('Y') ?> 
+    </footer>
+    
+</html>
