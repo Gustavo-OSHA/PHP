5a8484529b213c1d2d126d27ed54099050d32185
Gustavo de Oliveira Silva Hilario Amorim <guhilario@gmail.com>
2024-07-01 10:29:13 -0300

    Comentarios

---
+++ basico/comentarios.php
@@ -0,0 +1,19 @@
+<div class="titulo">Comentário PHP</div>
+
+<?php 
+echo "Estou no PHP";
+// echo "Comentario";
+# echo "Comentario";
+# echo // "Comentario";
+// echo #"Comentario";
+?>
+
+<p>Depois do primeiro bloco PHP</p>
+
+<?php 
+/*
+    Linha  1 do comentario
+    Linha  2 do comentario
+    Linha  3 do comentario
+*/
+?>
